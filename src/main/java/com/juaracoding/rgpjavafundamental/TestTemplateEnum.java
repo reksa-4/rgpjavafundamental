04081b57636e16a90fdbecc2045c79a3d784dd2e
reksa <reksa.its44@gmail.com>
2024-12-14 09:22:31 +0700

    Test Template

---
+++ src/main/java/com/juaracoding/rgpjavafundamental/TestTemplateEnum.java
@@ -0,0 +1,17 @@
+package com.juaracoding.rgpjavafundamental;
+
+
+/*
+IntelliJ IDEA 2024.3.1 (Community Edition)
+Build #IC-243.22562.145, built on December 9, 2024
+@Author REKSA GAMA P
+Java Developer
+Created on 14/12/2024 9:20
+@Last Modified 14/12/2024 9:20
+Version 1.0
+*/
+
+public enum TestTemplateEnum {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/com/juaracoding/rgpjavafundamental/TestTemplateInterface.java
@@ -0,0 +1,17 @@
+package com.juaracoding.rgpjavafundamental;
+
+
+/*
+IntelliJ IDEA 2024.3.1 (Community Edition)
+Build #IC-243.22562.145, built on December 9, 2024
+@Author REKSA GAMA P
+Java Developer
+Created on 14/12/2024 9:19
+@Last Modified 14/12/2024 9:19
+Version 1.0
+*/
+
+public interface TestTemplateInterface {
+    public static void main(String[] args) {
+    }
+}
