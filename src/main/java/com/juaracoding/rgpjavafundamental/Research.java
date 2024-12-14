aa1c0470515e952b8f119958bd6cc4f3d56779ce
reksa <reksa.its44@gmail.com>
2024-12-14 12:03:59 +0700

    Task Praktikum 1 dan Penambahan Statement Dependency di pom.xml

---
+++ src/main/java/com/juaracoding/rgpjavafundamental/Research.java
@@ -0,0 +1,148 @@
+package com.juaracoding.rgpjavafundamental;
+
+/*
+IntelliJ IDEA 2024.3.1 (Community Edition)
+Build #IC-243.22562.145, built on December 9, 2024
+@Author REKSA GAMA P
+Java Developer
+Created on 14/12/2024 10:12
+@Last Modified 14/12/2024 10:12
+Version 1.0
+*/
+
+import java.util.*;
+
+public class Research {
+    public static void main(String[] args) {
+
+//        int intLoop = 0;
+//
+//        for (int i = 0; i < 10 ; i++) {
+//                    intLoop++;
+//            for (int j = 0; j < 20; j++) {
+//                if (j==10) {
+//                    break;
+//                }
+//                intLoop++;
+//            }
+//            System.out.println(intLoop);
+//        }
+
+//        boolean isRemainderOn = true;
+//        int j = 1200;
+//        String strCuaca = "Hujan";
+//
+//        String strExec = "";
+//
+//        if(isRemainderOn) {
+//            strExec = "1";
+//            System.out.println("INI TRUE REMINDER !!");
+//        }
+//        else if(j>1000){
+//            strExec = "2";
+//            System.out.println("INI J > 1000 !!");
+//        }
+//        else if(strCuaca.equals("Hujan")){
+//            strExec = "3";
+//            System.out.println("SEKARANG SEDANG HUJAN !!");
+//        }
+//        else{
+//            strExec = "4";
+//            System.out.println("TIDAK ADA KONDISI YANG TERPENUHI");
+//        }
+//        System.out.println(strExec);
+
+//        if( ( 5 > 3) && (7 !=2))
+//        {
+//            System.out.println("OK");
+//        }
+//        else if((4 == 4) || !( 3 > 2))
+//        {
+//            System.out.println("YES");
+//        }
+//        else
+//        {
+//            System.out.println("NO");
+//        }
+
+//        int intLoop = 0;
+//        boolean isGood = true;
+//        do {
+//            intLoop++;
+//            continue;
+//        }
+//            while (!isGood) ;
+//            System.out.println(isGood);
+
+//        String strThird="Welcome";
+//        System.out.println(strThird.substring(1,5));
+
+//        String strSambung = "8" + 3;
+//        System.out.println(strSambung);
+
+//        String a = "TEA";
+//        String b = "TOE";
+//
+//        System.out.println(b.compareTo(a))  ;
+
+//        int panjang = 0;
+//        Scanner sn = new Scanner(System.in);
+//        panjang = sn.nextInt();
+//        System.out.println(panjang);
+
+//        int intLoop = 0;
+//
+//        for (int i = 0; i < 5; i++ ){
+//                intLoop++;
+//                continue;
+//        }
+//        System.out.println(intLoop);
+
+//        int intChoose = 3; intChoose = 2;
+//
+//        switch (intChoose)
+//        {
+//            case 1:
+//                System.out.println("Ini satu "); break;
+//            case 2:
+//                System.out.println("Ini dua "); break;
+//            default:
+//                System.out.println("Selain satu dan dua"); break;
+//        }
+
+//        int intLoop = 0;
+//
+//        for (int i = 0; i < 5; i++){
+//                intLoop++;
+//                if(intLoop>5)
+//                {
+//                    break;
+//                }
+//            System.out.println(intLoop);
+//        }
+
+//        boolean isRemainderOn = "true";
+//        int intVal = 1200;
+//        String strCuaca = "Hujan";
+//        String strExec = "";
+//        if(intVal >1500)
+//            System.out.println(intVal);
+//        System.out.println(strCuaca);
+
+//        Random rand = new Random ();
+//
+//        for (int i = 0; i < 10; i++)
+//        {
+//            System.out.println("Random Number "+i+" -> "+rand.nextInt(70,100));
+//        }
+
+        int intLoop = 0;
+        boolean isGood = true;
+        do {
+                intLoop++;
+                continue;
+        }while (!isGood);
+        System.out.println(intLoop);
+
+    }
+}
